d7d2025cebf9b1edb2372b29435d30c1439dc13b
Lyu Pengfei <lv.pengfei@oriental-pearl.cn>
2016-11-25 10:46:46 +0800

    add some schema

--- routes/index.ts
+++ routes/index.ts
@@ -1,13 +1,15 @@
 ///<reference path="../typings/express/express.d.ts"/>
 import * as express from "express";
-
+import Request = express.Request;
+import Response = express.Response;
+import NextFunction = express.NextFunction;
 
 let router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+router.get('/', function (req: Request, res: Response, next: NextFunction) {
+	res.render('index', {title: 'Express'});
 });
 
 export = router;
--- routes/users.ts
+++ routes/users.ts
@@ -1,13 +1,16 @@
 ///<reference path="../typings/express/express.d.ts"/>
 import * as express from "express";
+import Request = express.Request;
+import Response = express.Response;
+import NextFunction = express.NextFunction;
 
 
 let router = express.Router();
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	res.send('respond with a resource');
+router.get('/', function(req: Request, res: Response, next: NextFunction) {
+	res.send('respond with a users resource');
 });
 
 export = router;
